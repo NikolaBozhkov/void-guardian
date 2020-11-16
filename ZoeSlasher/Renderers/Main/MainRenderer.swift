d290a0c16da4f8215dac673069f94c02ca1fc78f
Nikola Bozhkov <bozhkov.nikola@gmail.com>
2020-11-16 13:22:55 +0200

    icons added

--- ZoeSlasher/Renderers/Main/MainRenderer.swift
+++ ZoeSlasher/Renderers/Main/MainRenderer.swift
@@ -372,7 +372,7 @@ extension MainRenderer: MTKViewDelegate {
         
         loadNoiseTextures(forAspectRatio: Float(aspectRatio), resolution: Float(size.height))
         
-//        Recorder.CaptureRect.size = sceneSize
+//        Recorder.CaptureRect.size = [700, 700]
 //        Recorder.CaptureRect.origin = -Recorder.CaptureRect.size / 2
 //        recorder.configure(withResolution: Int32(size.height), filePath: "demo1")
     }
